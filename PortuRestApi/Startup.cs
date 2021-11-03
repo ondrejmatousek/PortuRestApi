5a645edf953b81f6e4f47768295834896f924668
pcACERvrsovice <ondrejmatousek@email.cz>
2021-11-03 10:23:39 +0100

    swagger

--- PortuRestApi/Startup.cs
+++ PortuRestApi/Startup.cs
@@ -32,6 +32,11 @@ namespace PortuRestApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PortuRestApi");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
